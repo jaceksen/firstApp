485f0e77ee2ba3398a72a413eca881c64f79e81d
jaceksen <jaceksen@gmail.com>
2017-02-25 05:31:01 +0100

    170225_2

--- app/src/main/java/pl/jaceksen/firstapp/MainActivity.java
+++ app/src/main/java/pl/jaceksen/firstapp/MainActivity.java
@@ -2,12 +2,23 @@ package pl.jaceksen.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txtDisplay= (TextView) findViewById(R.id.txtDisplay);
+        txtDisplay.setText("Tekst from Main.");
+    }
+
+    public void buClick(View view) {
+        TextView txtDisplay= (TextView) findViewById(R.id.txtDisplay);
+        txtDisplay.setText("Przycisk był kliknięty.");
     }
 }
