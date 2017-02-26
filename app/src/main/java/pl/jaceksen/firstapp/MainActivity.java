1cea5ee44f3739ebe82730c312b90551e4b114af
jaceksen <jaceksen@gmail.com>
2017-02-26 01:23:50 +0100

    170225_2

--- app/src/main/java/pl/jaceksen/firstapp/MainActivity.java
+++ app/src/main/java/pl/jaceksen/firstapp/MainActivity.java
@@ -1,13 +1,15 @@
 package pl.jaceksen.firstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtDisplay;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView txtDisplay= (TextView) findViewById(R.id.txtDisplay);
         txtDisplay.setText("Tekst from Main.");
+        //button2
+//        button2.setText("Tekst z layoutu");
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView txtDisplay= (TextView) findViewById(R.id.txtDisplay);
+                txtDisplay.setText("Kliknięcie z kodu.");
+            }
+        });
     }
 
     public void buClick(View view) {
         TextView txtDisplay= (TextView) findViewById(R.id.txtDisplay);
         txtDisplay.setText("Przycisk był kliknięty.");
+//        txtDisplay.setText("aaaaaa");
     }
 }
